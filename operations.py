637c7c321f3b62ab703866795c8a2607f59f5d5e
João Victor Vieira Barros <joaovieirab147@gmail.com>
2026-07-12 20:57:57 -0300

    Documentation of list function and update_description script v1.0

--- operations.py
+++ operations.py
@@ -11,7 +11,7 @@ def create_task (agenda, description, id=1):
     Parameters:
         - agenda:       The agenda dictionary. contains the information of all taks linked until present.
         - description:  The task description. consists of a string that detail the task.
-        - id:           The identification tag. consists of an int number for position of task.
+        - id:           The identification tag. consists of an int number for task position.
     -------------------------------------------------------------------------------------------------------
     Retrun: Void. Update the agenda by insertining the new task in the agenda.
 
@@ -36,8 +36,31 @@ def create_task (agenda, description, id=1):
     agenda[new_task["id"]] = new_task
 
 def list_tasks(agenda):
-    print(ID.ljust(3), DESCRIPTION.ljust(20), STATUS.ljust(15))
-    print("---------------------------------------------")
+    """
+    Lists the tasks in agenda as a table format.
+    ---------------------------------------------------
+    Parameters:
+        - agenda:       The agenda dictionary. contains the information of all taks linked until present.
+    ---------------------------------------------------
+    Return: Void. List the data information of all tasks in agenda until present.
+    """
+    print(ID.ljust(3), DESCRIPTION.ljust(50), STATUS.ljust(25))
+    print("------------------------------------------------------------------")
     for key in agenda:
-        print(f"{agenda[key]['id']:<3} {agenda[key]['description']:<20} {agenda[key]['status']:<15}")
+        print(f"{agenda[key]['id']:<3} {agenda[key]['description']:<50} {agenda[key]['status']:<25}")
         
+def update_description(agenda, tag, new_description):
+    """
+    Updates the description of an especific task, witch its position is informed as an parameter 'tag'.
+    ------------------------------------------------------------------------------------
+    Parameters:
+        - agenda:       The agenda dictionary. contains the information of all taks linked until present.
+        - tag:          The identification tag. consists of an int number for task position.
+        - description:  The task description. consists of a string that detail the task.
+    ------------------------------------------------------------------------------------
+    Return: Void. Updates the task description positional informed by 'tag' parameter.
+
+    """
+    agenda[tag]["description"] = new_description
+    agenda[tag]["UpdatedAt"] = datetime.now()
+
